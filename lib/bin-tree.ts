bf542ace4c560bf321b37a3af85ec8666a28c2bb
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-11-05 21:48:07 +0100

    added impl and tests

--- lib/bin-tree.ts
+++ lib/bin-tree.ts
@@ -1,3 +1,149 @@
-export class BinTree {
+interface RecursionFreeBinTree<T> {
+    leftChild?: RecursionFreeBinTree<T>;
+    rightChild?: RecursionFreeBinTree<T>;
+    nodeData: T;
+}
 
+export class BinTree<T> {
+    private _nodeData: T;
+    private _parent?: BinTree<T>;
+    private _leftChild?: BinTree<T>;
+    private _rightChild?: BinTree<T>;
+
+    private constructor(nodeData: T, parent?: BinTree<T>) {
+        this._nodeData = nodeData;
+        this._parent = parent;
+    }
+
+    //<editor-fold desc="getters and setters">
+    get nodeData(): T {
+        return this._nodeData;
+    }
+
+    set nodeData(value: T) {
+        this._nodeData = value;
+    }
+
+    get parent(): BinTree<T> | undefined {
+        return this._parent;
+    }
+
+    set parent(value: BinTree<T> | undefined) {
+        this._parent = value;
+    }
+
+    get leftChild(): BinTree<T> | undefined {
+        return this._leftChild;
+    }
+
+    set leftChild(value: BinTree<T> | undefined) {
+        this._leftChild = value;
+        if (value) {
+            value.parent = this;
+        }
+    }
+
+    get rightChild(): BinTree<T> | undefined {
+        return this._rightChild;
+    }
+
+    set rightChild(value: BinTree<T> | undefined) {
+        this._rightChild = value;
+        if (value) {
+            value.parent = this;
+        }
+    }
+
+//</editor-fold>
+
+    public static init<R>(rootData: R): BinTree<R> {
+        return new BinTree<R>(rootData);
+    }
+
+    public createLeftChild(childData: T): void {
+        this._leftChild = new BinTree<T>(childData, this);
+    }
+
+    public createRightChild(childData: T): void {
+        this._rightChild = new BinTree<T>(childData, this);
+    }
+
+    public isRoot(): boolean {
+        //return !!this._parent;
+        return this._parent === undefined;
+    }
+
+    public isLeaf(): boolean {
+        //return !(this._leftChild || this._rightChild);
+        return this._leftChild === undefined && this._rightChild === undefined;
+    }
+
+    public getSubTreeInOrder(): BinTree<T>[] {
+        if (this.isLeaf()) {
+            return Array.of(this);
+        } else {
+            const lefts = this._leftChild ? this._leftChild.getSubTreeInOrder() : [];
+            const rights = this._rightChild ? this._rightChild.getSubTreeInOrder() : [];
+            const root: BinTree<T>[] = [this];
+            return lefts.concat(root).concat(rights);
+        }
+    }
+
+    public getSubTreePreOrder(): BinTree<T>[] {
+        if (this.isLeaf()) {
+            return [this];
+        } else {
+            const lefts = this._leftChild ? this._leftChild.getSubTreePreOrder() : [];
+            const rights = this._rightChild ? this._rightChild.getSubTreePreOrder() : [];
+            const root: BinTree<T>[] = [this];
+            return root.concat(lefts).concat(rights);
+        }
+    }
+
+    public map(fn: (n: T) => T): void {
+        this._nodeData = fn(this.nodeData);
+        if (this._leftChild) {
+            this._leftChild.map(fn);
+        }
+        if (this._rightChild !== undefined) {
+            this._rightChild.map(fn);
+        }
+    }
+
+    public getSubtreeSize(): number {
+        const leftSize = this._leftChild ? this._leftChild.getSubtreeSize() : 0;
+        const rightSize = this._rightChild ? this._rightChild.getSubtreeSize() : 0;
+        return 1 + leftSize + rightSize;
+    }
+
+    public getCurrentDepth(): number {
+        let result = 0;
+        let parent = this.parent;
+        while (parent) {
+            result++;
+            parent = parent.parent;
+        }
+        return result;
+    }
+
+    public toNonRecursive(): RecursionFreeBinTree<T> {
+        const leftChild = this._leftChild ? this._leftChild.toNonRecursive() : undefined;
+        const rightChild = this._rightChild ? this._rightChild.toNonRecursive() : undefined;
+        return {
+            nodeData: this.nodeData,
+            leftChild: leftChild,
+            rightChild: rightChild
+        };
+    }
+
+    public static fromNonRecursive<R>(nonRecursive: RecursionFreeBinTree<R>): BinTree<R> {
+        const node = new BinTree(nonRecursive.nodeData);
+        if (nonRecursive.leftChild) {
+            node.leftChild = BinTree.fromNonRecursive(nonRecursive.leftChild);
+        }
+        if (nonRecursive.rightChild) {
+            node.rightChild = BinTree.fromNonRecursive(nonRecursive.rightChild);
+        }
+        return node;
+    }
 }
--- test/bin-tree.test.ts
+++ test/bin-tree.test.ts
@@ -1,109 +1,104 @@
 import {BinTree} from "../lib/bin-tree";
 
-test('sample test', () => {
-    const bintree: BinTree = new BinTree();
-    expect(bintree).toBeTruthy();
+test('should create a root with data', () => {
+    const bintree: BinTree<string> = BinTree.init('test');
+    expect(bintree).toBeDefined();
+    expect(bintree.isLeaf()).toBe(true);
+    expect(bintree.isRoot()).toBe(true);
 });
 
-// test('should create a root with data', () => {
-//     const bintree: BinTree<string> = BinTree.init('test');
-//     expect(bintree).toBeDefined();
-//     expect(bintree.isLeaf()).toBe(true);
-//     expect(bintree.isRoot()).toBe(true);
-// });
-//
-// test('should add left and right child and set current node as parent of them', () => {
-//     const bintree: BinTree<string> = BinTree.init('test');
-//     bintree.createLeftChild('leftchild');
-//     bintree.createRightChild('rightchild');
-//     const leftChild: BinTree<string> | undefined = bintree.leftChild;
-//     const rightChild: BinTree<string> = bintree.rightChild as BinTree<string>;
-//     expect(bintree.isLeaf()).toBe(false);
-//     expect(leftChild).toBeDefined();
-//     expect(leftChild ? leftChild.isLeaf() : false).toBe(true);
-//     expect(rightChild).toBeDefined();
-//     expect(rightChild.isLeaf()).toBe(true);
-//     expect(rightChild.parent).toBe(bintree);
-// });
-//
-// //Tree
-// //        1
-// //    2       3
-// //  4   5
-// //In Order: 4 2 5 1 3
-// //Pre Order: 1 2 4 5 3
-// test('in order and pre order', () => {
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     const lc = bintree.leftChild as BinTree<number>;
-//     lc.createLeftChild(4);
-//     lc.createRightChild(5);
-//     const inOrder = bintree.getSubTreeInOrder().map(n => n.nodeData);
-//     const preOrder = bintree.getSubTreePreOrder().map(n => n.nodeData);
-//     expect(inOrder).toEqual([4, 2, 5, 1, 3]);
-//     expect(preOrder).toEqual([1, 2, 4, 5, 3]);
-// });
-//
-// test('json stringify should fail on nodes with children', () => {
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     const fn = () => JSON.stringify(bintree);
-//     expect(fn).toThrow();
-// });
-//
-// test('mapping method should map each node', () => {
-//     const mappingFn = (x: number) => x * 3;
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     bintree.map(mappingFn);
-//     const lc = bintree.leftChild as BinTree<number>;
-//     const rc = bintree.rightChild as BinTree<number>;
-//     expect(bintree.nodeData).toEqual(3);
-//     expect(lc.nodeData).toEqual(6);
-//     expect(rc.nodeData).toEqual(9);
-// });
-//
-// test('corect depth of node', () => {
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     const lc = bintree.leftChild as BinTree<number>;
-//     expect(bintree.getCurrentDepth()).toEqual(0);
-//     expect(lc.getCurrentDepth()).toEqual(1);
-// });
-//
-// test('to non recursive should be stringifiable and parsable', () => {
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     const nonRecursive = bintree.toNonRecursive();
-//     expect(nonRecursive).toBeDefined();
-//
-//     const json = JSON.stringify(nonRecursive);
-//     expect(json).toBeDefined();
-//
-//     const parsed = JSON.parse(json);
-//     expect(parsed).toBeDefined();
-//
-//     const tree = BinTree.fromNonRecursive(parsed) as BinTree<number>;
-//     expect(tree.nodeData).toEqual(bintree.nodeData);
-//     const btLc = bintree.leftChild as BinTree<number>;
-//     const lc = tree.leftChild as BinTree<number>;
-//     expect(btLc.nodeData).toEqual(lc.nodeData);
-//
-//     expect(lc.parent).toBe(tree);
-// });
-//
-// test('should return the correct subtree size', () => {
-//     const bintree: BinTree<number> = BinTree.init(1);
-//     bintree.createLeftChild(2);
-//     bintree.createRightChild(3);
-//     const lc = bintree.leftChild as BinTree<number>;
-//     lc.createLeftChild(4);
-//     lc.createRightChild(5);
-//
-//     expect(bintree.getSubtreeSize()).toEqual(5);
-// });
+test('should add left and right child and set current node as parent of them', () => {
+    const bintree: BinTree<string> = BinTree.init('test');
+    bintree.createLeftChild('leftchild');
+    bintree.createRightChild('rightchild');
+    const leftChild: BinTree<string> | undefined = bintree.leftChild;
+    const rightChild: BinTree<string> = bintree.rightChild as BinTree<string>;
+    expect(bintree.isLeaf()).toBe(false);
+    expect(leftChild).toBeDefined();
+    expect(leftChild ? leftChild.isLeaf() : false).toBe(true);
+    expect(rightChild).toBeDefined();
+    expect(rightChild.isLeaf()).toBe(true);
+    expect(rightChild.parent).toBe(bintree);
+});
+
+//Tree
+//        1
+//    2       3
+//  4   5
+//In Order: 4 2 5 1 3
+//Pre Order: 1 2 4 5 3
+test('in order and pre order', () => {
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    const lc = bintree.leftChild as BinTree<number>;
+    lc.createLeftChild(4);
+    lc.createRightChild(5);
+    const inOrder = bintree.getSubTreeInOrder().map(n => n.nodeData);
+    const preOrder = bintree.getSubTreePreOrder().map(n => n.nodeData);
+    expect(inOrder).toEqual([4, 2, 5, 1, 3]);
+    expect(preOrder).toEqual([1, 2, 4, 5, 3]);
+});
+
+test('json stringify should fail on nodes with children', () => {
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    const fn = () => JSON.stringify(bintree);
+    expect(fn).toThrow();
+});
+
+test('mapping method should map each node', () => {
+    const mappingFn = (x: number) => x * 3;
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    bintree.map(mappingFn);
+    const lc = bintree.leftChild as BinTree<number>;
+    const rc = bintree.rightChild as BinTree<number>;
+    expect(bintree.nodeData).toEqual(3);
+    expect(lc.nodeData).toEqual(6);
+    expect(rc.nodeData).toEqual(9);
+});
+
+test('corect depth of node', () => {
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    const lc = bintree.leftChild as BinTree<number>;
+    expect(bintree.getCurrentDepth()).toEqual(0);
+    expect(lc.getCurrentDepth()).toEqual(1);
+});
+
+test('to non recursive should be stringifiable and parsable', () => {
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    const nonRecursive = bintree.toNonRecursive();
+    expect(nonRecursive).toBeDefined();
+
+    const json = JSON.stringify(nonRecursive);
+    expect(json).toBeDefined();
+
+    const parsed = JSON.parse(json);
+    expect(parsed).toBeDefined();
+
+    const tree = BinTree.fromNonRecursive(parsed) as BinTree<number>;
+    expect(tree.nodeData).toEqual(bintree.nodeData);
+    const btLc = bintree.leftChild as BinTree<number>;
+    const lc = tree.leftChild as BinTree<number>;
+    expect(btLc.nodeData).toEqual(lc.nodeData);
+
+    expect(lc.parent).toBe(tree);
+});
+
+test('should return the correct subtree size', () => {
+    const bintree: BinTree<number> = BinTree.init(1);
+    bintree.createLeftChild(2);
+    bintree.createRightChild(3);
+    const lc = bintree.leftChild as BinTree<number>;
+    lc.createLeftChild(4);
+    lc.createRightChild(5);
+
+    expect(bintree.getSubtreeSize()).toEqual(5);
+});
